319028a23a9ca518bfd98358b8634f689c17917e
geo <spybdai@gmail.com>
2016-04-14 01:36:42 +0800

    finally finsihed

--- git_push.sh
+++ git_push.sh
@@ -2,11 +2,15 @@
 set des [lindex $argv 0]
 set local [lindex $argv 1]
 set remote [lindex $argv 2]
-spawn echo "pushing update from local '$des' to remote '$local/$remote'..."
+#spawn process command should be hidden
+spawn -noecho echo "pushing update from local '$des' to remote '$local/$remote'..."
 expect eof
-spawn git push $des $local:$remote
+spawn -noecho git push $des $local:$remote
+# the following info should be hidden
+log_user 0
 expect "Username"
 send "ispcfs@gmail.com\r"
 expect "Password"
 send "$env(GIT_PASSWD)\r"
+log_user 1
 expect eof
