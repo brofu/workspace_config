3dee8f6519d7a32199a24a98fc6292959b6f14e8
geo <spybdai@gmail.com>
2016-04-14 00:17:54 +0800

    finish git_push

--- git_push.sh
+++ git_push.sh
@@ -1,1 +1,12 @@
-git push $1 $2:$3
+#!/usr/bin/expect
+set des [lindex $argv 0]
+set local [lindex $argv 1]
+set remote [lindex $argv 2]
+spawn echo "pushing update from local '$des' to remote '$local/$remote'..." > /dev/null
+expect eof
+spawn git push $des $local:$remote
+expect "Username"
+send "ispcfs@gmail.com\r"
+expect "Password"
+send "352213os839836\r"
+expect eof
