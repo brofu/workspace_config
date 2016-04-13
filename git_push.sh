5a8d396d683c9320560054956bb83dca12cfda88
geo <spybdai@gmail.com>
2016-04-14 02:09:32 +0800

    use hidden un

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/expect
+# this script is used for git push without input username and password every time
 set des [lindex $argv 0]
 set local [lindex $argv 1]
 set remote [lindex $argv 2]
@@ -9,7 +10,7 @@ spawn -noecho git push $des $local:$remote
 # the following info should be hidden
 log_user 0
 expect "Username"
-send "ispcfs@gmail.com\r"
+send "$env(GIT_USERNAME)\r"
 expect "Password"
 send "$env(GIT_PASSWD)\r"
 log_user 1
