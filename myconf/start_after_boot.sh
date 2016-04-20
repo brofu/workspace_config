784ca822153050dbbefbe33358b748591176725d
geo <spybdai@gmail.com>
2016-04-20 12:44:33 +0800

    add updated booting scripts

--- myconf/start_after_boot.sh
+++ myconf/start_after_boot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-ERROR_LOG_FOLDER=$HOME/changfu_config
+CHANGFU_HOME=/home/changfu
+ERROR_LOG_FOLDER=$CHANGFU_HOME/changfu_config
 # start the shadowsocks after boot
 sslocal -c ./private_conf/.shadowsocks/config.json 1>/dev/null 2>$ERROR_LOG_FOLDER/shadowsock_error.log &
