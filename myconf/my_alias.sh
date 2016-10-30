d79a945a319d85060b77e6e077188a2969cbf946
shaocf <shaocf@garena.com>
2016-10-30 22:29:29 +0800

    .bashrc

--- myconf/my_alias.sh
+++ myconf/my_alias.sh
@@ -13,3 +13,12 @@ alias gitpush='$CHANGFU_SHELL/git_push.sh'
 # alias for ssh
 alias ssh142='$CHANGFU_SHELL/ssh_auto_login.sh maiziedu 192.168.1.142 1234 /var/www/code.maiziedu'
 alias ssh16='$CHANGFU_SHELL/ssh_auto_login.sh root 192.168.1.16 1234 /var/www/code.maiziedufps'
+
+# alias for google
+alias chrome='google-chrome-stable & 1>/dev/null 2>&1' 
+
+# alias for subline text 3
+alias subl='subl & 1>/dev/null 2>&1'
+
+# alias for test servers
+alias ssh_test_id='ssh ld-shaocf@'
